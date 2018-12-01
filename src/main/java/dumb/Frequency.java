3288dce270619f7be67332e28d91e2558622b710
Mathieu Pouëdras <7620812+mathieupouedras@users.noreply.github.com>
2018-12-01 11:23:43 +0100

    in progress

---
+++ src/main/java/dumb/Frequency.java
@@ -0,0 +1,48 @@
+package dumb;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frequency {
+
+    public static void main(String[] args) {
+        Frequency frequency = new Frequency();
+        String[] lines = frequency.getContent("input.txt");
+        int count = 0;
+        for (String line: lines) {
+            String operator = line.substring(0, 1);
+            int amount = Integer.valueOf(line.substring(1, line.length() - 1));
+            switch (operator) {
+                case "+": count += amount;
+                case "-": count -= amount;
+            }
+        }
+
+    }
+
+    private String[] getContent(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        try {
+            List<String> lines = new ArrayList<String>();
+            BufferedReader bufferedReader =  new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+            String[] results = new String[lines.size()];
+
+            return lines.toArray(results);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
